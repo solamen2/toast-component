15ed34b020d42cf8927a334d0f6f717d7d264bea
Edward Younskevicius <eyounskevicius@fullsail.com>
2023-06-29 14:17:09 -0700

    Remove unused import

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Button from '../Button';
-import Toast from '../Toast';
 import ToastShelf from '../ToastShelf/ToastShelf';
 
 import styles from './ToastPlayground.module.css';
